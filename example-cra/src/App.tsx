64841dd550ec280d26de88756d97836f2108bf4e
Hartaithan <hartaithan@gmail.com>
2023-09-30 15:26:54 +0900

    separate vite and cra examples

--- example-cra/src/App.tsx
+++ example-cra/src/App.tsx
@@ -1,7 +1,10 @@
+import { Text } from "tetch-ui-kit";
+
 function App() {
   return (
     <>
       <h1>Hello World!</h1>
+      <Text>Hello World!</Text>
     </>
   );
 }
---
+++ example-vite/src/App.tsx
@@ -0,0 +1,12 @@
+import { Text } from "tetch-ui-kit";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <Text>Hello World!</Text>
+    </>
+  );
+}
+
+export default App;
