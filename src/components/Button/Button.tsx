b89de5e7744182c254734f207e038fbe7e32d7d5
Hartaithan <hartaithan@gmail.com>
2023-09-30 15:27:49 +0900

    fix test components

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,8 +3,8 @@ import React, { FC } from "react";
 export type ButtonProps = React.ComponentPropsWithoutRef<"button">;
 
 const Button: FC<ButtonProps> = (props) => {
-  const { children } = props;
-  return <p>{children}</p>;
+  const { children, ...rest } = props;
+  return <button {...rest}>{children}</button>;
 };
 
 export default Button;
--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -3,8 +3,8 @@ import React, { FC } from "react";
 export type TextProps = React.ComponentPropsWithoutRef<"p">;
 
 const Text: FC<TextProps> = (props) => {
-  const { children } = props;
-  return <p>{children}</p>;
+  const { children, ...rest } = props;
+  return <p {...rest}>{children}</p>;
 };
 
 export default Text;
