ef1d1eee1c955ca924fac303a5db3dedcaad5c98
Hartaithan <hartaithan@gmail.com>
2023-10-04 18:59:06 +0900

    style button component

--- example-vite/src/App.tsx
+++ example-vite/src/App.tsx
@@ -1,10 +1,15 @@
-import { Text } from "tetch-ui-kit";
+import { Button, Text } from "tetch-ui-kit";
 
 function App() {
   return (
     <>
       <h1>Hello World!</h1>
       <Text>Hello World!</Text>
+      <Button size="sm">Hello World!</Button>
+      <Button size="md">Hello World!</Button>
+      <Button size="lg">Hello World!</Button>
+      <Button size="xl">Hello World!</Button>
+      <Button size="xxl">Hello World!</Button>
     </>
   );
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,38 @@
 import * as React from "react";
+import styles from "./Button.module.css";
+import { ButtonSize } from "./Button.types";
+import clsx from "../../utils/classes";
+import { ColorProps } from "../../styles/colors/types";
+import { colors } from "../../styles";
 
-export type ButtonProps = React.ComponentPropsWithoutRef<"button">;
+export interface ButtonProps
+  extends React.ComponentPropsWithoutRef<"button">,
+    ColorProps {
+  size?: ButtonSize;
+}
 
 const Button: React.FC<ButtonProps> = (props) => {
-  const { children, ...rest } = props;
-  return <button {...rest}>{children}</button>;
+  const {
+    className,
+    children,
+    size = "md",
+    c = "brand",
+    cd = 500,
+    style,
+    ...rest
+  } = props;
+  return (
+    <button
+      className={clsx(className, styles.button, styles[`button-${size}`])}
+      style={{
+        backgroundColor: c && cd && colors[c][cd],
+        ...style,
+      }}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
